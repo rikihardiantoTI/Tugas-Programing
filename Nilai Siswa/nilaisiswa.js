673c20206f64a4d59382bb0cfc487766d1b738d3
Riki Hardianto <rikihardiantoti@gmail.com>
2025-09-21 19:05:33 +0700

    Tugas

---
+++ Nilai Siswa/nilaisiswa.js
@@ -0,0 +1,17 @@
+ 
+let matematika = 85;
+let bahasaInggris = 90;
+let ipa = 78;
+
+ 
+let nilaiRataRata = (matematika + bahasaInggris + ipa) / 3;
+
+ 
+console.log("Nilai Rata-Rata:", nilaiRataRata);
+console.log("Tipe Data:", typeof nilaiRataRata);
+ 
+if (nilaiRataRata >= 80) {
+    console.log("Status: Lulus");
+} else {
+    console.log("Status: Tidak Lulus");
+}
